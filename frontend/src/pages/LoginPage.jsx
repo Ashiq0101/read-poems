bf552e1883090132394fa5dcd152bf1381c6891f
Ashiq0101 <aaashiq1100@gmail.com>
2026-02-28 00:31:35 +0600

    pass

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -7,9 +7,7 @@ function LoginPage() {
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
-  const handleLogin = (e) => {
-    e.preventDefault(); // prevent page reload
-
+  const handleLogin = () => {
     if (email === "abc@gmail.com" && password === "123") {
       navigate("/upload");
     } else {
@@ -19,48 +17,47 @@ function LoginPage() {
 
   return (
     <div className="login-page">
+
       <div className="login-overlay">
+
         <div className="login-card">
 
-          <h2 className="login-title">💖 User’s Diary</h2>
+          <h2 className="login-title">
+            💖 User’s Diary
+          </h2>
+
           <p className="login-subtitle">
             Enter your credentials to continue ✨
           </p>
 
-          {/* Use form instead of div */}
-          <form onSubmit={handleLogin}>
+          <div className="login-input-group">
+            <input
+              type="email"
+              placeholder="Email Address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
 
-            <div className="login-input-group">
-              <input
-                type="email"
-                placeholder="Email Address"
-                value={email}
-                required
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
+          <div className="login-input-group">
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
 
-            <div className="login-input-group">
-              <input
-                type="password"
-                placeholder="Password"
-                value={password}
-                required
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-
-            <button className="login-btn" type="submit">
-              🔐 Login
-            </button>
-
-          </form>
+          <button className="login-btn" onClick={handleLogin}>
+            🔐 Login
+          </button>
 
           <Link to="/" className="back-link">
             ← Back to Home
           </Link>
 
         </div>
+
       </div>
     </div>
   );
